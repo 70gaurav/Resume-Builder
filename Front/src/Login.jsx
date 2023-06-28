92035f893b4e833ffc87f0f7d910e59836346736
70gaurav <gk17265@gmail.com>
2023-06-28 18:32:13 +0530

    forms

--- Front/src/Login.jsx
+++ Front/src/Login.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='Login'>
+      <form action="">
+        <div className=''>
+          <h3>USERNAME</h3>
+          <input type="text" placeholder='Enter Your Username' />
+        </div>
+        <div className=''>
+          <h3>PASSWORD</h3>
+        <input type="password" placeholder="Enter Your Password" />
+
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+
+    </div>
   )
 }
 
---
+++ Front/src/Signup.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='signup'>
+        <form action="">
+            <div>
+                <h3>E-MAIL</h3>
+                <input type="email" placeholder='ENTER YOUR E-MAIL'/>
+            </div>
+            <div>
+                <h3>USERNAME</h3>
+                <input type="text" placeholder='ENTER YOUR USERNAME'/>
+            </div>
+            <div>
+                <h3>PASSWORD</h3>
+                <input type="password" placeholder='ENTER YOUR PASSWORD'/>
+            </div>
+            <div>
+                <input type="submit" />
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Signup

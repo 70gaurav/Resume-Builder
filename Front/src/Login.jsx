ae76eab74267bad77abc44fb4f8adb271f2d82a4
70gaurav <gk17265@gmail.com>
2023-07-01 22:30:48 +0530

    typewriter

--- Front/src/Login.jsx
+++ Front/src/Login.jsx
@@ -1,37 +1,51 @@
-import React from 'react'
-import dummy from "./slice/images/1.png"
+import React from 'react';
+import dummy from "./slice/images/1.png";
+import Typewriter from 'typewriter-effect';
 
 function Login() {
   return (
     <div className='login'>
       <div className='dummy-main'>
-      <div className='dummy'>
-        <img src={dummy} alt="" />
-
-      </div>
+        <div className='dummy'>
+          <img src={dummy} alt="" />
+        </div>
       </div>
       
       <div className='form-main'>
-      <div><h1>RESU<span>MIFY</span></h1></div>
-      <form action="">
-        <div className=''>
-          <h3>USERNAME</h3>
-          <input type="text" placeholder='Enter Your Username' />
-        </div>
-        <div className=''>
-          <h3>PASSWORD</h3>
-        <input type="password" placeholder="Enter Your Password" />
-
-        </div>
+       
         <div>
-          <button type='submit'>Log In</button>
+        <div className="type">
+          <Typewriter
+            options={{
+              loop: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("<h1>RESU<span>MIFY</span></h1>")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("<h1>CREATE YOUR <br/><span>RESUME</span> NOW <span>!</span></h1>")
+                .start();
+            }}
+          />
         </div>
-      </form>
+        </div>
+        <form action="">
+          <div className=''>
+            <h3>USERNAME</h3>
+            <input type="text" placeholder='Enter Your Username' />
+          </div>
+          <div className=''>
+            <h3>PASSWORD</h3>
+            <input type="password" placeholder="Enter Your Password" />
+          </div>
+          <div>
+            <button type='submit'>Log In</button>
+          </div>
+        </form>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Login
-
+export default Login;
